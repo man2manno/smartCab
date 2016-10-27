a8a3dfeac659bd27092ea0a1edc1258703e5697f
Anthony Manno <man2manno@users.noreply.github.com>
2016-10-27 13:09:41 -0500

    Updated Agent.py
    The smart cab will not follow all the rules of the road.

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -24,18 +24,25 @@ class LearningAgent(Agent):
 
         # TODO: Update state
 	self.state = (inputs['light'],self.next_waypoint)
-	print 'Current State: {}'.format(self.state)
+	#print 'Current State: {}'.format(self.state)
         
         # TODO: Select action according to your policy
         #action = random.choice([None,'forward','left','right'])
-	if inputs['light'] != 'red':
-		action = self.state[1]
+        if inputs['light'] == 'green' and (inputs['oncoming'] != 'forward' or inputs['oncoming'] != 'right') and self.state[1] == 'left':
+            action = 'left'
+	elif inputs['light'] == 'green' and self.state[1] == 'right':
+		action = 'right'
+        elif inputs['light'] == 'green' and self.state[1] == 'forward':
+            action = 'forward'
+        elif inputs['light'] == 'red' and (self.state[1] == 'forward' or self.state[1] == 'left'):
+            action = None
+        elif inputs['light'] == 'red' and self.state[1] == 'right' and inputs['oncoming'] != 'left' and inputs['left'] != 'forward':
+            action = 'right'
 	else:
-		action = None
+            action = self.state[1]
 
         # Execute action and get reward
         reward = self.env.act(self, action)
-
         # TODO: Learn policy based on state, action, reward
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
@@ -47,14 +54,14 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=3, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=1, display=True)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
-    sim.run(n_trials=1)  # run for a specified number of trials
+    sim.run(n_trials=10)  # run for a specified number of trials
     # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
 
 
